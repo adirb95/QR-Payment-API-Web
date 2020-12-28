5e4029ecfd4f748588348ac0bb9de0ef99cd3966
adirb95 <65300958+adirb95@users.noreply.github.com>
2020-12-28 23:36:08 +0200

    Initial commit

---
+++ src/test/java/qr/Main/QrPaymentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package qr.Main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrPaymentProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
