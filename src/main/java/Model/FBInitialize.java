351242bc09c4e49698ccf5294c8d4b6bbaf30921
adirb95 <65300958+adirb95@users.noreply.github.com>
2021-01-13 02:01:06 +0200

    last update before production new repackaged as jar

--- src/main/java/Model/FBInitialize.java
+++ src/main/java/Model/FBInitialize.java
@@ -1,13 +1,17 @@
 package Model;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Arrays;
 import java.util.Objects;
 
 @Service
@@ -16,16 +20,18 @@ public class FBInitialize {
     @PostConstruct
     public void initialize() {
         try {
-            String fileName = "jsonfile/qr-database-a6078-firebase-adminsdk-885ht-5dd2f2d0f1.json";
+            String fileName = "src/main/java/jsonfile/qr-database-a6078-firebase-adminsdk-885ht-5dd2f2d0f1.json";
             ClassLoader classLoader = getClass().getClassLoader();
             /*File file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).getFile());*/
-            File file = new File("src/main/java/jsonfile/qr-database-a6078-firebase-adminsdk-885ht-5dd2f2d0f1.json");
-            FileInputStream serviceAccount = new FileInputStream(file);
+            File file = new File(fileName);
+            FileInputStream serviceAccount = new FileInputStream("target/classes/qr-database-a6078-firebase-adminsdk-885ht-5dd2f2d0f1.json");
+            GoogleCredentials googleCred=GoogleCredentials.fromStream(serviceAccount);
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(googleCred)
                     .setDatabaseUrl("https://qr-database-a6078.firebaseio.com")
                     .build();
-            FirebaseApp.initializeApp(options);
+
+                    FirebaseApp.initializeApp(options);
         } catch (Exception e) {
             e.printStackTrace();
         }
